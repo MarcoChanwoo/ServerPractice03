8318c1e0aa2f77855bcd320f4fa417da1dd98706
Marco <cwhong8087@gmail.com>
2023-09-20 20:55:16 +0900

    update

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,6 +1,6 @@
 import Post from '../../modules/post';
 import mongoose from 'mongoose';
-import Joi from 'joi';
+import Joi from '../../../node_modules/joi/lib/index';
 
 const { ObjectId } = mongoose.Types;
 
@@ -93,9 +93,27 @@ export const remove = async (ctx) => {
 
 /*  데이터 수정
     PATCH /api/posts/:id
+    {
+        title: '수정',
+        body: '수정 내용',
+        tags: ['태그1', '태그2']
+    }
 */
 export const update = async (ctx) => {
     const { id } = ctx.params;
+    // write에서 사용한 schema와 비슷하나, required()가 없음
+    const schema = Joi.object().keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+    // 검증 실패인 경우 에러처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+        ctx.body = result.error;
+        return;
+    }
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true,
@@ -111,124 +129,3 @@ export const update = async (ctx) => {
         ctx.throw(500, e);
     }
 };
-
-// let postId = 1; // id의 초기값
-
-// // posts 배열 초기 데이터
-// const posts = [
-//     {
-//         id: 1,
-//         title: '제목',
-//         body: '내용',
-//     },
-// ];
-
-// /*  포스트 작성
-//     POST /api/posts
-//     {title, body}
-// */
-// export const write = (ctx) => {
-//     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있음
-//     const { title, body } = ctx.request.body;
-//     postId += 1;
-//     const post = { id: postId, title, body };
-//     posts.push(post);
-//     ctx.body = post;
-// };
-
-// /*  포스트 목록 조회
-//     GET /api/posts
-// */
-// export const list = (ctx) => {
-//     ctx.body = posts;
-// };
-
-// /*  특정 포스트 조회
-//     GET /api/posts/:id
-// */
-// export const read = (ctx) => {
-//     const { id } = ctx.params;
-//     // 주어진 id값으로 포스트를 찾음
-//     // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
-//     // 비교할 p.id 값을 문자열로 변환해야 함
-//     const post = posts.find((p) => p.id.toString() === id);
-//     // 포스트가 없으면 오류를 반환함
-//     if (!post) {
-//         ctx.status = 404;
-//         ctx.body = {
-//             message: '포스트가 존재하지 않습니다.',
-//         };
-//         return;
-//     }
-//     ctx.body = post;
-// };
-
-// /*  특정 포스트 제거
-//     DELETE /api/posts/:id
-// */
-// export const remove = (ctx) => {
-//     const { id } = ctx.params;
-//     // 해당 id를 가진 post가 몇 번째인지 확인함
-//     const index = posts.findIndex((p) => p.id.toString() === id);
-//     // 포스트가 없으면 오류를 반환
-//     if (index === -1) {
-//         ctx.status = 404;
-//         ctx.body = {
-//             message: '포스트가 존재하지 않습니다.',
-//         };
-//         return;
-//     }
-//     posts.splice(index, 1);
-//     ctx.status = 204; // Conflict
-// };
-
-// /*  포스트 수정(교체)
-//     PUT /api/posts/:id
-//     {title, body}
-// */
-// export const replace = (ctx) => {
-//     // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용함
-//     const { id } = ctx.params;
-//     // 해당 id를 가진 post가 몇 번째인지 확인함
-//     const index = posts.findIndex((p) => p.id.toString() === id);
-//     // 포스트가 없으면 오류를 반환
-//     if (index === -1) {
-//         ctx.status = 404;
-//         ctx.body = {
-//             message: '포스트가 존재하지 않습니다.',
-//         };
-//         return;
-//     }
-//     // 전체 객체를 덮어 씌움
-//     // => id를 제외한 기존 정보를 날리고, 객체를 새로 만듦
-//     posts[index] = {
-//         id,
-//         ...ctx.request.body,
-//     };
-//     ctx.body = posts[index];
-// };
-
-// /*  포스트 수정(특정 필드 변경)
-//     PATCH /api/posts/:id
-//     {title, body}
-// */
-// export const update = (ctx) => {
-//     // PATCH 메서드는 주어진 필드만 교체함
-//     const { id } = ctx.params;
-//     // 해당 id를 가진 post가 몇 번째인지 확인함
-//     const index = posts.findIndex((p) => p.id.toString() === id);
-//     // 포스트가 없으면 오류를 반환
-//     if (index === -1) {
-//         ctx.status = 404;
-//         ctx.body = {
-//             message: '포스트가 존재하지 않습니다.',
-//         };
-//         return;
-//     }
-//     // 기존 값에 정보를 덮어 씌움
-//     posts[index] = {
-//         ...posts[index],
-//         ...ctx.request.body,
-//     };
-//     ctx.body = posts[index];
-// };
